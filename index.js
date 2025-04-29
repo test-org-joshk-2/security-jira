dd419d993528e6bb8c4a7c60cdc26e7d1c123d7b
joshkay10 <josh.kay@outlook.com>
2025-04-30 11:06:02 +1200

    MEOW-1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 jQuery.fn.copyText = function(options) {
-	// BAD may evaluate `options.sourceSelector` as HTML
+	// BAD may evaluate `options.sourceSelector` as HTMLasdasd
 	var source = jQuery(options.sourceSelector),
 	    text = source.text();
 	jQuery(this).text(text);
